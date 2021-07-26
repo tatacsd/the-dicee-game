69a81d43201506cf8fdbbeb4bd3821c75167fbf9
Thays Casado <thayscasado@hotmail.com>
2021-07-25 23:45:09 -0700

    added function to get a random number

--- index.js
+++ index.js
@@ -1,6 +1,12 @@
 // set random number between 1 and 6
-const max = 6;
-const min = 1;
-const randomNumber1 = Math.floor(Math.random() * (max + 1 - min) + min);
+function getRandomNumber() {
+  const max = 6;
+  const min = 1;
+  return Math.floor(Math.random() * (max + 1 - min) + min);
+}
 
-console.log(randomNumber1);
+let img1 = `dice${getRandomNumber()}.png`;
+let img2 = `dice${getRandomNumber()}.png`;
+
+console.log(img1);
+console.log(img2);
